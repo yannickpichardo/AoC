fa26afd44fbb6206d5b98c0b073a1fd38a63431f
yannickpichardo <yannickpichardo@gmail.com>
2024-12-02 10:59:23 +0100

    day02: tried to vectorize:

--- 2024/day02/main.py
+++ 2024/day02/main.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 
 
 def get_safety_level(x: np.ndarray):
@@ -22,6 +23,18 @@ def get_safety_level_dampened(x: np.ndarray):
     return False
 
 
+def get_safety_level_dampened_vectorized(x: np.ndarray):
+    if get_safety_level(x):
+        return True
+
+    # Create an identity matrix for each row with all options:
+    masks = np.eye(len(x), dtype=bool)
+    # Create all the possible dampened dampened_arrays
+    # This however will lead to an overestimation
+    dampened_arrays = [x[~mask] for mask in masks]
+    return any(get_safety_level(x_dampened) for x_dampened in dampened_arrays)
+
+
 if __name__ == "__main__":
     file_path = r"day02input.txt"
     data = []
@@ -31,5 +44,14 @@ if __name__ == "__main__":
             data.append(np.array([float(value) for value in line.split()]))
     safety_levels = [get_safety_level(row) for row in data]
     print(sum(safety_levels))
+    start_loop = time.time()
     safety_levels_dampened = [get_safety_level_dampened(row) for row in data]
     print(sum(safety_levels_dampened))
+    print(f"Time: {time.time() - start_loop}")
+    start_vect = time.time()
+    safety_levels_dampened_vector = [
+        get_safety_level_dampened_vectorized(row) for row in data
+    ]
+    print(sum(safety_levels_dampened_vector))
+    print(f"Time Vect: {time.time() - start_vect}")
+    # No Noticeable difference between times.
