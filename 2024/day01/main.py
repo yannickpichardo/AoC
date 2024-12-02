6215a87528290a51685d70c62dab86b787443835
yannickpichardo <yannickpichardo@gmail.com>
2024-12-02 11:04:33 +0100

    day01: cleaned up day1 code

--- 2024/day01/main.py
+++ 2024/day01/main.py
@@ -1,26 +1,27 @@
 import numpy as np
 
 
-def get_distance_sum(x: np.ndarray, y: np.ndarray):
+def get_distance_sum(x: np.ndarray, y: np.ndarray) -> int:
+    # Sort arrays
+    x = np.sort(x)
+    y = np.sort(y)
     distance = np.abs(x - y)
-    return np.sum(distance)
+    return int(np.sum(distance))
 
 
-def get_similarity_score(x: np.ndarray, y: np.ndarray):
+def get_similarity_score(x: np.ndarray, y: np.ndarray) -> int:
     # Create set of occurrences of y
     unique_counts = np.unique(y, return_counts=True)
     # Create dictionary for easy indexing
     y_counts = dict(zip(*unique_counts))
-    score = sum(value * y_counts.get(value, 0) for value in x)
+    score = int(sum(value * y_counts.get(value, 0) for value in x))
     return score
 
 
 if __name__ == "__main__":
     txt_path = r"day01input.txt"
     col1, col2 = np.loadtxt(txt_path, unpack=True)
-    ordered_col1 = np.sort(col1)
-    ordered_col2 = np.sort(col2)
-    distance_sum = get_distance_sum(x=ordered_col1, y=ordered_col2)
+    distance_sum = get_distance_sum(x=col1, y=col2)
     print(distance_sum)
-    sim_score = get_similarity_score(ordered_col1, ordered_col2)
+    sim_score = get_similarity_score(x=col1, y=col2)
     print(sim_score)
